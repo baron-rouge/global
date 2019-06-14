776f1a6138331146b0434ee7ea2e972607bdea0b
tony48 <35108858+tony48@users.noreply.github.com>
2019-06-14 16:49:01 -0400

    ON MERGE
    YEET

---
+++ Red Birds/Assets/Scripts/flecheControl.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(MeshCollider))]
+
+public class flecheControl : MonoBehaviour
+{
+
+    private Vector3 screenPoint;
+    private Vector3 offset;
+    public Transform cone;
+    public string axis;
+
+    void OnMouseDown()
+    {
+        screenPoint = Camera.main.WorldToScreenPoint(gameObject.transform.position);
+
+        offset = gameObject.transform.position - Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, screenPoint.z));
+
+    }
+
+    void OnMouseDrag()
+    {
+        Vector3 curScreenPoint = new Vector3(Input.mousePosition.x, Input.mousePosition.y, screenPoint.z);
+
+        Vector3 curPosition = Camera.main.ScreenToWorldPoint(curScreenPoint) + offset;
+        if (axis == "x")
+            transform.position = new Vector3((float)System.Math.Round(curPosition.x, 1), transform.position.y, transform.position.z);
+        else if (axis == "xy")
+            transform.position = new Vector3((float)System.Math.Round(curPosition.x, 1), transform.position.y, (float)System.Math.Round(curPosition.y, 1));
+
+    }
+
+}
