9f8b1cb1b50ee678ce3ec98f49f49cf7933c2fa3
tony48 <antolinares01@gmail.com>
2019-06-14 16:40:47 -0400

    yeet

---
+++ Red Birds/Assets/Scripts/ProceduralCone.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProceduralCone : MonoBehaviour
+{
+    public Transform flecheBr;
+    public Transform flecheTr;
+    Vector3 lastArrowPosBr;
+    Vector3 lastArrowPosTr;
+    MeshFilter mf;
+    Mesh mesh;
+    MeshCollider mc;
+    // Start is called before the first frame update
+    void Start()
+    {
+        mf = gameObject.GetComponent<MeshFilter>();
+        mesh = mf.mesh;
+        lastArrowPosBr = flecheBr.localPosition;
+        lastArrowPosTr = flecheTr.localPosition;
+        mc = gameObject.GetComponent<MeshCollider>();
+    }
+    
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 arrowPosBr = flecheBr.localPosition;
+        Vector3 arrowPosTr = flecheTr.localPosition;
+        if (arrowPosBr.x == lastArrowPosBr.x && arrowPosTr.x == lastArrowPosTr.x)
+            return;
+        
+        MeshUtils.CreateCone(2f, flecheBr.GetComponent<MeshRenderer>().bounds.center.x - flecheBr.GetComponent<MeshRenderer>().bounds.extents.x - transform.position.x, transform.position.x - flecheTr.GetComponent<MeshRenderer>().bounds.center.x - flecheTr.GetComponent<MeshRenderer>().bounds.extents.x, 18, mesh);
+        mc.sharedMesh = mesh;
+        lastArrowPosTr = arrowPosTr;
+        lastArrowPosBr = arrowPosBr;
+    }
+
+}
