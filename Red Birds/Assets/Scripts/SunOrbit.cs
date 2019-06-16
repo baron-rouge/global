bb65fd57d5c0e1e72cd1bf5a075feb9196e88e18
tony48 <35108858+tony48@users.noreply.github.com>
2019-06-16 07:45:54 -0400

    on rassemble tout

---
+++ Red Birds/Assets/Scripts/SunOrbit.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SunOrbit : MonoBehaviour
+{
+    public static float centerx;
+    public static float centerz;
+    Vector3 center;
+    void Start()
+    {
+        
+        centerx = Terrain.activeTerrain.terrainData.size.x / 2f;
+        centerz = Terrain.activeTerrain.terrainData.size.y / 2f;
+        center = new Vector3(centerx, 0, centerz);
+    }
+
+    public float theta = 0.1f;
+
+
+    void Update()
+    {
+        this.transform.RotateAround(center, Vector3.right, theta);
+    }
+}
